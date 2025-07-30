9135d4e3026260d10f7b64c37884bf429108c6bc
soban-amir-qbatch <soban.amir@qbatch.com>
2025-07-30 16:25:50 +0500

    init

---
+++ app/components/navbar.tsx
@@ -0,0 +1,27 @@
+import { ArrowRight, ChevronDown, ChevronUp } from 'lucide-react';
+
+import { Link } from 'react-router'
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <nav className="flex items-center justify-around w-full px-6 py-4 bg-primary">
+  
+        <div className="flex items-center space-x-8">
+          <Link to="\"><img src="/image.png" alt="Logo" className="h-10 w-10" /></Link>
+          <a href="#" className="text-secondary hover:text-blue-600">Pricing</a>
+          <a href="#" className="text-secondary hover:text-blue-600">About</a>
+          <a href="#" className="text-secondary hover:text-blue-600">Blog</a>
+          <a href="#" className="text-secondary hover:text-blue-600">Wall of Love</a>
+          {/* TODO: Make interactive */}
+          <a href="#" className="text-secondary hover:text-blue-600 flex items-end gap-0.5">Resources <ChevronDown size={20} /></a>
+        </div>
+
+        <div className="flex items-center space-x-6">
+          <a href="/contact" className="text-secondary hover:text-blue-600">Sign In</a>
+          <a href="/login" className="text-gray-100 flex items-end gap-0.5 transition-all duration-150 hover:translate-x-1">Request Demo <ArrowRight className='text-blue-600' size={20} /> </a>
+        </div>
+      
+    </nav>
+  )
+}
--- app/root.tsx
+++ app/root.tsx
@@ -1,14 +1,16 @@
+import "./app.css";
+
 import {
-  isRouteErrorResponse,
   Links,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  isRouteErrorResponse,
 } from "react-router";
 
+import Navbar from "./components/navbar";
 import type { Route } from "./+types/root";
-import "./app.css";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -42,7 +44,12 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
-  return <Outlet />;
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  )
 }
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
